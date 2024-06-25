b0b139c25eb2809d818596000a219b38a2e54456
gohil rohit <gohilrohit309@gmail.com>
2024-06-25 19:18:29 +0530

    add style in login-form

--- E-Commerce/src/Components/Login.jsx
+++ E-Commerce/src/Components/Login.jsx
@@ -35,19 +35,23 @@ const Login = () => {
 
     return (
         <>
-            <h2 className="text-center text-5xl font-semibold my-5">Login Form</h2>
-            <div className="flex justify-center mt-10">
-                <form method="post" onSubmit={(e) => handleSubmit(e)}>
-                    <label htmlFor="">Enter Your Username :- </label>
-                    <input type="text" placeholder="Enter Your Username" name="username" onChange={(e) => getInputValue(e)} /><br /><br />
-                    <label htmlFor="">Enter Your Email :- </label>
-                    <input type="text" placeholder="Enter Your Email" name="email" onChange={(e) => getInputValue(e)} /><br /><br />
-                    <label htmlFor="">Enter Your Password :- </label>
-                    <input type="text" placeholder=" Enter Your Password" name="pass" onChange={(e) => getInputValue(e)} /> <br /><br />
-                    <button type="submit">Login</button>
-                </form>
+            <div className="linear-bg h-screen">
+            <h2 className="text-center text-5xl font-semibold">Login Form</h2>
+            <div className="max-w-4xl mx-auto px-4 border">
+                <div className="flex justify-center p-5">
+                    <form method="post" onSubmit={(e) => handleSubmit(e)}>
+                        <label className="text-xl font-medium mb-2 inline-block text-white">Enter Your Username :- </label>
+                        <input type="text" placeholder="Enter Your Username" className="inline-block w-full pr-6 border-white border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="username" onChange={(e) => getInputValue(e)} /><br /><br />
+                        <label className="text-xl font-medium mb-2 inline-block text-white">Enter Your Email :- </label>
+                        <input type="text" placeholder="Enter Your Email" className="inline-block w-full pr-6 border-white border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="email" onChange={(e) => getInputValue(e)} /><br /><br />
+                        <label className="text-xl font-medium mb-2 inline-block text-white">Enter Your Password :- </label>
+                        <input type="text" placeholder=" Enter Your Password" className="inline-block w-full pr-6 border-white border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="pass" onChange={(e) => getInputValue(e)} /> <br /><br />
+                        <button type="submit" className="bg-blue-600 text-white text-lg font-medium px-7 py-1 mt-8">Login</button>
+                    </form>
+                </div>
             </div>
-            <ToastContainer/>
+            </div>
+            <ToastContainer />
         </>
     )
 }
