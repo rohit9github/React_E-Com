498a86c3e8335bd069507b9c5c4b670f1ff3e256
gohil rohit <gohilrohit309@gmail.com>
2024-06-26 18:52:42 +0530

    apply validation in login and register form

--- E-Commerce/src/Components/Login.jsx
+++ E-Commerce/src/Components/Login.jsx
@@ -6,7 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
 
-    let [loginUser, setLoginUser] = useState({})
+    let [loginUser, setLoginUser] = useState({});
+    let [error, setError] = useState({});
 
     let navigate = useNavigate()
 
@@ -14,42 +15,91 @@ const Login = () => {
         let name = e.target.name;
         let value = e.target.value;
         setLoginUser({ ...loginUser, [name]: value })
+        if (name === "username") {
+            if (value === "") {
+                setError({ ...error, nameError: "Please Enter Your Username" })
+            }
+            else {
+                setError({ ...error, nameError: "" })
+            }
+        }
+        else if (name === "email") {
+            if (value === "") {
+                setError({ ...error, emailError: "Please Enter Your Email" })
+            }
+            else {
+                setError({ ...error, emailError: "" })
+            }
+        }
+        else if (name === "pass") {
+            if (value === "") {
+                setError({ ...error, passError: "Please Enter Your Password" })
+            }
+            else {
+                setError({ ...error, passError: "" })
+            }
+        }
     }
 
     let handleSubmit = (e) => {
         e.preventDefault()
-
-        const getUsers = JSON.parse(localStorage.getItem("user")) || [];
-
-        const registerUser = getUsers.some((user) => user.username === loginUser.username && user.email === loginUser.email && user.pass === loginUser.pass);
-
-        if (registerUser) {
-            alert("Login Successfully");
-            navigate("/")
+        if (loginUser.username === undefined) {
+            setError({ ...error, nameError: "Please Enter Your Username" })
+        }
+        else if (loginUser.email === undefined) {
+            setError({ ...error, emailError: "Please Enter Your Email" })
+        }
+        else if (loginUser.pass === undefined) {
+            setError({ ...error, passError: "Please Enter Your Password" })
         }
         else {
-            alert("Please First Register");
-            navigate("/register")
+            const getUsers = JSON.parse(localStorage.getItem("user")) || [];
+
+            const registerUser = getUsers.some((user) => user.username === loginUser.username && user.email === loginUser.email && user.pass === loginUser.pass);
+
+            if (registerUser) {
+                alert("Login Successfully");
+                navigate("/")
+            }
+            else {
+                alert("Please First Register");
+                navigate("/register")
+            }
+            setError({})
         }
+
     }
 
     return (
         <>
             <div className="linear-bg h-screen flex justify-center items-center">
-            <div className="max-w-4xl mx-auto px-4 border bg-white py-20 rounded-md">
-            <h2 className="text-center text-5xl font-semibold ">Login Form</h2>
-                <div className="flex justify-center px-16 mt-10">
-                    <form method="post" onSubmit={(e) => handleSubmit(e)} >
-                        <label className="text-xl font-medium mb-2 inline-block">Enter Your Username :- </label>
-                        <input type="text" placeholder="Enter Your Username" className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="username" onChange={(e) => getInputValue(e)} /><br /><br />
-                        <label className="text-xl font-medium mb-2 inline-block">Enter Your Email :- </label>
-                        <input type="text" placeholder="Enter Your Email" className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="email" onChange={(e) => getInputValue(e)} /><br /><br />
-                        <label className="text-xl font-medium mb-2 inline-block">Enter Your Password :- </label>
-                        <input type="text" placeholder=" Enter Your Password" className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="pass" onChange={(e) => getInputValue(e)} /> <br /><br />
-                        <button type="submit" className="linear-bg w-full text-white text-2xl font-semibold px-7 py-3 mt-8 rounded-md">Login</button>
-                    </form>
+                <div className="max-w-xl mx-auto px-4 border bg-white py-20 rounded-md">
+                    <h2 className="text-center text-5xl font-semibold ">Login Form</h2>
+                    <div className="flex justify-center px-16 mt-10">
+                        <form method="post" onSubmit={(e) => handleSubmit(e)} >
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Username :- </label>
+                                <input type="text" placeholder="Enter Your Username" className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="username" onChange={(e) => getInputValue(e)} />
+                                <div>
+                                    <span>{error.nameError ? error.nameError : ""}</span>
+                                </div>
+                            </div>
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Email :- </label>
+                                <input type="text" placeholder="Enter Your Email" className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="email" onChange={(e) => getInputValue(e)} />
+                                <div>
+                                    <span>{error.emailError ? error.emailError : ""}</span>
+                                </div>
+                            </div>
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Password :- </label>
+                                <input type="text" placeholder=" Enter Your Password" className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " name="pass" onChange={(e) => getInputValue(e)} />
+                                <span>{error.passError ? error.passError : ""}</span>
+                            </div>
+                            <button type="submit" className="linear-bg w-full text-white text-2xl font-semibold px-7 py-3 mt-8 rounded-md">Login</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
             </div>
             <ToastContainer />
         </>
--- E-Commerce/src/Components/Register.jsx
+++ E-Commerce/src/Components/Register.jsx
@@ -5,22 +5,82 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
 
-    let [registerUser, setRegisterUser] = useState({})
+    let [registerUser, setRegisterUser] = useState({});
+    let [error, setError] = useState({});
 
 
     let getUserValue = (e) => {
         let name = e.target.name;
         let value = e.target.value;
-        setRegisterUser({ ...registerUser, [name]: value })
+        setRegisterUser({ ...registerUser, [name]: value });
+        if (name === "username") {
+            if (value === "") {
+                setError({ ...error, nameError: "Username Is Required" });
+            }
+            else {
+                setError({ ...error, nameError: "" })
+            }
+        }
+        else if (name === "email") {
+            if (value === "") {
+                setError({ ...error, emailError: "Email Is Required" });
+            }
+            else {
+                setError({ ...error, emailError: "" })
+            }
+        }
+        else if (name === "number") {
+            if (value === "") {
+                setError({ ...error, numberError: "Number Is Required" })
+            }
+            else {
+                setError({ ...error, numberError: "" });
+            }
+        }
+        else if (name === "pass") {
+            if (value === "") {
+                setError({ ...error, passError: "Password Is Required" })
+            }
+            else {
+                setError({ ...error, passError: "" })
+            }
+        }
+        else if (name === "confirmpass") {
+            if (value === "") {
+                setError({ ...error, confirmpassError: "Please Confirm Your Password" })
+            }
+            else {
+                setError({ ...error, confirmpassError: "" })
+            }
+        }
     }
 
     let handleSubmit = (e) => {
         e.preventDefault()
-        let users = JSON.parse(localStorage.getItem("user")) || [];
-        users.push(registerUser);
-        localStorage.setItem("user", JSON.stringify(users));
-        setRegisterUser({})
-        toast.success("Register SuccessFully")
+        if (registerUser.username === undefined) {
+            setError({ ...error, nameError: "Username Is Required" });
+        }
+        else if (registerUser.email === undefined) {
+            setError({ ...error, emailError: "Email Is Required" })
+        }
+        else if (registerUser.number === undefined) {
+            setError({ ...error, numberError: "Number Is Required" })
+        }
+        else if (registerUser.pass === undefined) {
+            setError({ ...error, passError: "Password Is Required" })
+        }
+        else if (registerUser.confirmpass === undefined) {
+            setError({ ...error, confirmpassError: "Please Confirm Your  Password" })
+        }
+        else {
+            let users = JSON.parse(localStorage.getItem("user")) || [];
+            users.push(registerUser);
+            localStorage.setItem("user", JSON.stringify(users));
+            setRegisterUser({})
+            toast.success("Register SuccessFully")
+            setError({})
+        }
+
     }
 
     return (
@@ -30,16 +90,42 @@ const Register = () => {
                     <h2 className="text-center text-5xl font-semibold my-5">Register Form</h2>
                     <div className="flex justify-center px-28 mt-10 ">
                         <form method="post" onSubmit={(e) => handleSubmit(e)}>
-                            <label className="text-xl font-medium mb-2 inline-block">Enter Your Name :- </label>
-                            <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Name" name="username" value={registerUser.username ? registerUser.username : ""} onChange={(e) => getUserValue(e)} /><br /><br />
-                            <label className="text-xl font-medium mb-2 inline-block">Enter Your Email :- </label>
-                            <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Email" name="email" value={registerUser.email ? registerUser.email : ""} onChange={(e) => getUserValue(e)} /><br /><br />
-                            <label className="text-xl font-medium mb-2 inline-block">Enter Your Number :-  </label>
-                            <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Number" name="number" value={registerUser.number ? registerUser.number : ""} onChange={(e) => getUserValue(e)} /><br /><br />
-                            <label className="text-xl font-medium mb-1 inline-block">Enter Your Password :-  </label>
-                            <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Password" name="pass" value={registerUser.pass ? registerUser.pass : ""} onChange={(e) => getUserValue(e)} /><br /><br />
-                            <label className="text-xl font-medium mb-1 inline-block">Confirm Password :- </label>
-                            <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Confirm Password" name="confirmpass" value={registerUser.confirmpass ? registerUser.confirmpass : ""} onChange={(e) => getUserValue(e)} /><br /> <br />
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Name :- </label>
+                                <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Name" name="username" value={registerUser.username ? registerUser.username : ""} onChange={(e) => getUserValue(e)} />
+                                <div>
+                                    <span>{error.nameError ? error.nameError : ""}</span>
+                                </div>
+                            </div>
+
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Email :- </label>
+                                <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Email" name="email" value={registerUser.email ? registerUser.email : ""} onChange={(e) => getUserValue(e)} />
+                                <div>
+                                    <span>{error.emailError ? error.emailError : ""}</span>
+                                </div>
+                            </div>
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Number :-  </label>
+                                <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Number" name="number" value={registerUser.number ? registerUser.number : ""} onChange={(e) => getUserValue(e)} />
+                                <div>
+                                    <span>{error.numberError ? error.numberError : ""}</span>
+                                </div>
+                            </div>
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Enter Your Password :-  </label>
+                                <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Enter Your Password" name="pass" value={registerUser.pass ? registerUser.pass : ""} onChange={(e) => getUserValue(e)} />
+                                <div>
+                                    <span>{error.passError ? error.passError : ""}</span>
+                                </div>
+                            </div>
+                            <div className="my-5">
+                                <label className="text-xl font-medium inline-block">Confirm Password :- </label>
+                                <input className="border-black inline-block w-full pr-6 border-b-2 py-2 ps-4 text-lg outline-none bg-transparent " type="text" placeholder="Confirm Your Password" name="confirmpass" value={registerUser.confirmpass ? registerUser.confirmpass : ""} onChange={(e) => getUserValue(e)} />
+                                <div>
+                                    <span>{error.confirmpassError ? error.confirmpassError : ""}</span>
+                                </div>
+                            </div>
                             <button type="submit" className="linear-bg w-full text-white text-2xl font-semibold px-7 py-3 mt-8 rounded-md">Submit</button>
                         </form>
                     </div>
